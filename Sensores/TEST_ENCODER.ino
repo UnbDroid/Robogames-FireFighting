1e80b84ee68267faf7df9c2599707f7c9d3b8725
Abdullah Zaiter <abdullah.zaiter@hotmail.com>
2017-03-18 12:55:55 -0300

    teste do encoder, funcionou em dois motores

---
+++ Sensores/TEST_ENCODER.ino
@@ -0,0 +1,22 @@
+#define Encoder1 12
+int z;
+void setup() {
+  pinMode(Encoder1, INPUT);
+  Serial.begin(9600);
+  z = 0;
+}
+
+void loop() {
+  int i=0, cont=0;
+  while(i<131){
+    while(cont < 1024){
+      if(digitalRead(Encoder1)){
+          cont+=1;
+        }
+      }
+      cont = 0;
+      i+=1;
+  }
+  Serial.println(z);
+  z+=1;
+}
