4d54c5117caabe423c1f03f684673067a5f2b4c4
abdullah-zaiter <abdullah.zaiter@hotmail.com>
2017-03-21 20:18:16 -0300

    os codigos do giroscopio do bruno

---
+++ Sensores/Giroscopio/Girosc_pio.ino
@@ -0,0 +1,26 @@
+#include <Wire.h>
+
+#define MOT_ESQ 3
+#define MOT_DIR 11
+
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+  // Configura o L3G4200 para 200, 500 ou 2000 graus/seg
+  setupL3G4200D(2000);
+  //Setup_PID(); 
+  // Aguarda a resposta do sensor
+  delay(1000); 
+}
+void loop()
+{
+  int start = millis();
+
+  while(millis() - start < 50)
+  {
+    update_gyro();
+  }
+  Serial.println(get_gyro());
+}
+
---
+++ Sensores/Giroscopio/PID.ino
@@ -0,0 +1,40 @@
+#include <PID_v1.h>
+
+double pot_esq, pot_dir;
+double gyro;
+double Kep = 1, Kei = 0, Ked = 0;
+double Kdp = 1, Kdi = 0, Kdd = 0;
+
+
+PID PID_ESQ(&gyro, &pot_esq, 0, Kep, Kei, Ked, REVERSE);
+PID PID_DIR(&gyro, &pot_dir, 0, Kdp, Kdi, Kdd, DIRECT);
+
+void Setup_PID()
+{
+  PID_ESQ.SetOutputLimits(110, 255);
+  PID_DIR.SetOutputLimits(110, 255);
+  PID_ESQ.SetSampleTime(100);
+  PID_DIR.SetSampleTime(100);
+  PID_ESQ.SetMode(AUTOMATIC);
+  PID_DIR.SetMode(AUTOMATIC);
+}
+
+void update_pid()
+{
+  update_gyro();
+  gyro = get_gyro();
+  PID_ESQ.Compute();
+  PID_DIR.Compute();
+}
+
+double potesq()
+{
+  return pot_esq;
+}
+
+double potdir()
+{
+  return pot_dir;
+}
+
+
