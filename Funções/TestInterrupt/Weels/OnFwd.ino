b45404b8d288a480dd1d6e07728a80afd66ba236
minibauchspiess <dbauchspiess@yahoo.com>
2017-04-07 16:54:11 -0300

    Add files via upload

---
+++ Funções/TestInterrupt/Weels/OnFwd.ino
@@ -0,0 +1,21 @@
+
+
+void OnFwd(int motor, int power){
+  if(motor==MOTOR_LEFT){
+    digitalWrite(MAL, 1);//(1+(power/abs(power)))/2);
+    digitalWrite(MBL, 0);//-(1+(power/abs(power)))/2);
+    digitalWrite(MOTORL_VEL, power);
+  }
+  else{
+    if(motor==MOTOR_RIGHT){
+      digitalWrite(MAR, 1);//(1+(power/abs(power)))/2);
+      digitalWrite(MBR, 0);//-(1+(power/abs(power)))/2);
+      digitalWrite(MOTORR_VEL, power);
+      dir = dir*(power/abs(power));
+    }
+    else{
+      //manda mensagem de erro
+    }
+  }
+}
+
