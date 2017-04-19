35cb2355acfc0071ea0c9b64eb9f5ce760fa13b9
minibauchspiess <dbauchspiess@yahoo.com>
2017-04-18 22:54:55 -0300

    ISeeFire

--- Sensores/ChamaMain/ChamaMain.ino
+++ Sensores/ChamaMain/ChamaMain.ino
@@ -1,25 +1,26 @@
 
 #define FLAME_PIN_AN  A0
-#define FLAME_PEAK 100
+#define FLAME_PEAK 60
 
 int sFlameLast=0, sFlameNow, flamePresence=0;
 
 void ISeeFire(){
-  /*flameLast=flameNow;*/
+  sFlameLast=sFlameNow;/**/
   sFlameNow=analogRead(FLAME_PIN_AN);
-  /*if((sFlameNow-sFlameLast)>FLAME_PEAK){
+  if((sFlameNow-sFlameLast)>FLAME_PEAK){
     flamePresence=1;
-  }*/
+  }/**/
 }
 
 void setup() {
   Serial.begin(9600);
   pinMode(FLAME_PIN_AN, INPUT);
+  sFlameNow=analogRead(FLAME_PIN_AN);
 }
 
 void loop() {
   ISeeFire();
   Serial.println(sFlameNow);
-  /*Serial.println(flamePresence);*/
+  Serial.println(flamePresence);/**/
   delay(50);
 }
